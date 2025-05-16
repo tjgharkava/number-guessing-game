74477b3b3b808581850558f4e8b0abae7138355e
Temo <tdzhgarkava@yahoo.com>
2025-05-16 15:06:33 +0400

    commit

--- src/main/java/ge/temo/Main.java
+++ src/main/java/ge/temo/Main.java
@@ -1,17 +1,56 @@
 package ge.temo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    public static void main(String[] args) {
+        System.out.println("Welcome to Number Guessing Game!");
+        do {
+            playGame();
+        } while(askToPlayAgain());
+        System.out.println("Thanks for Playing! See You Next Time!");
+    }
+
+    public static void playGame() {
+        System.out.print("Enter the number for range: ");
+        int range = scanner.nextInt();
+
+        int randomNumber = random.nextInt(range) + 1;
+        int maxAttempts = 3;
+        int attempts = 0;
+        boolean guessed = false;
+
+        while(attempts < maxAttempts) {
+            System.out.print("Your Guess: ");
+            int guess = scanner.nextInt();
+            attempts++;
+
+            if(guess < randomNumber) {
+                System.out.println("To Low!");
+            } else if (guess > randomNumber) {
+                System.out.println("To High!");
+            } else {
+                System.out.println("Congratulations! You won! You guessed in " + attempts + " attempts.");
+                guessed = true;
+                break;
+            }
         }
+
+        if(!guessed) {
+            System.out.println("You Lost! The number was " + randomNumber + ".");
+        }
+
+        System.out.println();
+    }
+
+    public static boolean askToPlayAgain() {
+        System.out.print("Wanna Play Again? (yes/no): ");
+        scanner.nextLine();
+        String answer = scanner.nextLine().trim().toLowerCase();
+
+        return answer.equals("yes");
     }
 }
